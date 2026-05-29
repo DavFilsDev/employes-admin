772da863aaaa3eda3cb2a62e7b716fbe03ce28ec
DavFilsDev <miharisoadavidfils@gmail.com>
2026-05-29 11:43:21 +0300

    feat: full CRUD employees module with React-Admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
+import { EmployeeList } from "./employees/EmployeeList";
+import { EmployeeCreate } from "./employees/EmployeeCreate";
+import { EmployeeEdit } from "./employees/EmployeeEdit";
+import { EmployeeShow } from "./employees/EmployeeShow";
+
 const dataProvider = jsonServerProvider("http://localhost:3002");
 
 export default function App() {
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="employees" />
+      <Resource
+        name="employees"
+        list={EmployeeList}
+        create={EmployeeCreate}
+        edit={EmployeeEdit}
+        show={EmployeeShow}
+      />
     </Admin>
   );
 }
